d9366a7f2e777a298d06e561a8d39e4df52b7a69
Sihan Ma <masihan0303@gmail.com>
2024-12-01 00:01:05 +0000

    Added JWT

--- movies-api/api/users/userModel.js
+++ movies-api/api/users/userModel.js
@@ -8,6 +8,30 @@ const UserSchema = new Schema({
   password: {type: String, required: true }
 });
 
+// Compare password
+UserSchema.methods.comparePassword = async function (candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
 
+// Find user by username
+UserSchema.statics.findByUserName = function (username) {
+  return this.findOne({ username: username });
+};
+
+// Pre-save hook to hash password
+UserSchema.pre('save', async function(next) {
+  const saltRounds = 10;
+  if (this.isModified('password') || this.isNew) {
+    try {
+      const hash = await bcrypt.hash(this.password, saltRounds);
+      this.password = hash;
+      next();
+    } catch (error) {
+      next(error);
+    }
+  } else {
+    next();
+  }
+});
 
 export default mongoose.model('User', UserSchema);
