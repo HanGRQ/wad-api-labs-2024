868c868118eee7019fd83444681baa192f2631fc
Sihan Ma <masihan0303@gmail.com>
2024-12-06 22:30:50 +0000

    Integration Lab - Integrate Users Endpoints

--- reactApp/src/api/movies-api.js
+++ reactApp/src/api/movies-api.js
@@ -3,4 +3,26 @@ export const getMovies = async () => {
       `https://api.themoviedb.org/3/discover/movie?api_key=e0a23ced8716630906cbfb3c490ac3e9&language=en-US&include_adult=false&page=1`
     )
     return response.json()
-  };
+};
+
+export const login = async (username, password) => {
+  const response = await fetch('http://localhost:8080/api/users', {
+      headers: {
+          'Content-Type': 'application/json'
+      },
+      method: 'post',
+      body: JSON.stringify({ username: username, password: password })
+  });
+  return response.json();
+};
+
+export const signup = async (username, password) => {
+  const response = await fetch('http://localhost:8080/api/users?action=register', {
+      headers: {
+          'Content-Type': 'application/json'
+      },
+      method: 'post',
+      body: JSON.stringify({ username: username, password: password })
+  });
+  return response.json();
+};
